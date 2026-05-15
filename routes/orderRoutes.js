7675d6f33a6305a33c3c7f6652ac0e0079db3bb8
Abhishek Raj <abhishekraj55224@gmail.com>
2026-05-15 21:35:37 +0530

    fixed product model duplication

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Order = require("../models/Order");
-const Product = require("../models/Product");
+const Product = require("../models/product");
 
 const Razorpay = require("razorpay");
 
