72f8220adf2cd95f236b07facd6bb39fd1ac8c25
Abhishek Raj <abhishekraj55224@gmail.com>
2026-06-02 14:51:27 +0530

    fix cors for local frontend

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://earthkind-frontend.vercel.app"
-    ],
+  "https://earthkind-frontend.vercel.app",
+  "http://localhost:5173"
+],
     credentials: true
   })
 );
