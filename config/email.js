8c9b9c6acfd6cfdf58d92f11cf47b52a58315968
Abhishek Raj <abhishekraj55224@gmail.com>
2026-06-02 23:47:56 +0530

    fixed mail transporter

--- config/email.js
+++ config/email.js
@@ -1,11 +1,36 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+
+  host: "smtp.gmail.com",
+
+  port: 465,
+
+  secure: true,
+
   auth: {
+
     user: process.env.EMAIL_USER,
+
     pass: process.env.EMAIL_PASS
+
   }
+
+});
+
+transporter.verify((error, success) => {
+
+  if (error) {
+
+    console.log("EMAIL CONFIG ERROR:");
+    console.log(error);
+
+  } else {
+
+    console.log("MAIL SERVER READY");
+
+  }
+
 });
 
 module.exports = transporter;
