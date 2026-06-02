c9fa9c6dbe6928fba0b3fa08a1949e7fb8d1c352
Abhishek Raj <abhishekraj55224@gmail.com>
2026-06-02 23:54:24 +0530

    fixed gmail smtp

--- config/email.js
+++ config/email.js
@@ -4,15 +4,21 @@ const transporter = nodemailer.createTransport({
 
   host: "smtp.gmail.com",
 
-  port: 465,
+  port: 587,
 
-  secure: true,
+  secure: false,
 
   auth: {
 
     user: process.env.EMAIL_USER,
 
     pass: process.env.EMAIL_PASS
+
+  },
+
+  tls: {
+
+    rejectUnauthorized: false
 
   }
 
